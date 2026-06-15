44e4cdc3607b04a1d54833f7fb1374c62124cace
JulienP36 <juju.poitreau@gmail.com>
2026-06-15 17:13:46 +0200

    final modifications before github pages

--- src/main.py
+++ src/main.py
@@ -3,13 +3,21 @@ from file_ops import clear_then_copy
 from page_generation import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_output = "./docs"
 dir_path_content = "./content"
 
 def main():
+    if len(sys.argv) == 1:
+        basepath = "/"
+    elif len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    else:
+        raise Exception("Too many arguments")
     
-    clear_then_copy(dir_path_static, dir_path_public)
-    generate_pages_recursive(dir_path_content, "template.html", dir_path_public)
+    print(f"sys.argv = {sys.argv}")
+    
+    clear_then_copy(dir_path_static, dir_path_output)
+    generate_pages_recursive(dir_path_content, "template.html", dir_path_output, basepath)
 
 if __name__ == "__main__":
     main()
--- src/page_generation.py
+++ src/page_generation.py
@@ -9,7 +9,7 @@ def get_file_content(file_path: str) -> str:
     else:
         raise FileNotFoundError(f'File not found at "{file_path}"')
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(from_path: str, template_path: str, dest_path: str, base_path: str) -> None:
     print(f'Generating page from "{from_path}" to "{dest_path}" using "{template_path}"')
 
     template_content = get_file_content(template_path)
@@ -19,6 +19,9 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     title = extract_title(markdown_content)
     template_content = template_content.replace("{{ Title }}", title, 1)
     template_content = template_content.replace("{{ Content }}", html_content, 1)
+    print(f"base_path: {base_path}")
+    template_content = template_content.replace('href="/', f'href="{base_path}')
+    template_content = template_content.replace('src="/', f'src="{base_path}')
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir != "":
@@ -27,7 +30,7 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     with open(dest_path, 'w') as dest_file:
         dest_file.write(template_content)
 
-def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str, base_path: str) -> None:
     if os.path.isdir(dir_path_content):
         dir_content = os.listdir(dir_path_content)
         for file in dir_content:
@@ -37,11 +40,11 @@ def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir
                 file_extension = pathlib.Path(file_path).suffix.lower()
                 if file_extension == ".md":
                     file_path_target = pathlib.Path(file_path_target).with_suffix(".html")
-                    generate_page(file_path ,template_path , file_path_target)
+                    generate_page(file_path ,template_path , file_path_target, base_path)
                 else:
                     print(f'Skipping "{file}" because it is not a markdown file')
             elif os.path.isdir(file_path):
                 dir_to_check = os.path.join(dir_path_content, file)
-                generate_pages_recursive(dir_to_check, template_path, file_path_target)
+                generate_pages_recursive(dir_to_check, template_path, file_path_target, base_path)
     else:
         raise Exception("The path provided is not a directory")
