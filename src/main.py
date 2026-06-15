5b3925901025419e3bbbd231f66cae511e8f912b
JulienP36 <juju.poitreau@gmail.com>
2026-06-15 16:25:23 +0200

    adding a recursive function to do the generation on the whole target directory

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
+import sys
 from file_ops import clear_then_copy
-from page_generation import generate_page
+from page_generation import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
 
 def main():
+    
     clear_then_copy(dir_path_static, dir_path_public)
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive(dir_path_content, "template.html", dir_path_public)
 
 if __name__ == "__main__":
     main()
--- src/page_generation.py
+++ src/page_generation.py
@@ -1,4 +1,4 @@
-import os
+import os, pathlib
 from markdown_ops_blocks import extract_title, markdown_to_html_node
 
 def get_file_content(file_path: str) -> str:
@@ -26,3 +26,22 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
     with open(dest_path, 'w') as dest_file:
         dest_file.write(template_content)
+
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+    if os.path.isdir(dir_path_content):
+        dir_content = os.listdir(dir_path_content)
+        for file in dir_content:
+            file_path = os.path.join(dir_path_content, file)
+            file_path_target = os.path.join(dest_dir_path, file)
+            if os.path.isfile(file_path):
+                file_extension = pathlib.Path(file_path).suffix.lower()
+                if file_extension == ".md":
+                    file_path_target = pathlib.Path(file_path_target).with_suffix(".html")
+                    generate_page(file_path ,template_path , file_path_target)
+                else:
+                    print(f'Skipping "{file}" because it is not a markdown file')
+            elif os.path.isdir(file_path):
+                dir_to_check = os.path.join(dir_path_content, file)
+                generate_pages_recursive(dir_to_check, template_path, file_path_target)
+    else:
+        raise Exception("The path provided is not a directory")
